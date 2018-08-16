9e9d698222e32ec5fdd56c426ec8b89850ae1c48
ankitagarwal04 <ankit.mrka4@gmail.com>
2018-08-17 00:54:22 +0530

    added article user association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,8 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
+    # hardcoded until the authentication is implemented
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
