6405c5a2306a66a0ec7a57e1c88d8ff78d79f72c
ankitagarwal04 <ankit.mrka4@gmail.com>
2018-08-18 16:00:43 +0530

    added edit and update functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    debugger
     if @user.save
       flash[:success] = "Welcome to Alpha Blog #{@user.username}"
       redirect_to articles_path
@@ -14,7 +13,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your user updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
