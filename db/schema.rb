808528a2346688699bde5692ed1e202ea95cc113
ankitagarwal04 <ankit.mrka4@gmail.com>
2018-08-24 02:01:52 +0530

    give rights to admin for all actions over articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180816194933) do
+ActiveRecord::Schema.define(version: 20180823194707) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180816194933) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
